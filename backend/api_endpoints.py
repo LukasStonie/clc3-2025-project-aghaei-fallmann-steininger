b8a9309260aa0d406472aa036506772e5ea500d1
Lukas Steininger <s2110458027@students.fh-hagenberg.at>
2026-01-29 18:24:52 +0100

    local development of backend api

---
+++ backend/api_endpoints.py
@@ -0,0 +1,60 @@
+import random
+import numpy as np
+
+from fastapi import FastAPI, Request, Response, HTTPException
+from pydantic import BaseModel
+from prometheus_client import Counter, Histogram, Gauge, generate_latest
+import time, psutil
+
+
+
+class TicketPurchase(BaseModel):
+    user_id: str
+    quantity: int
+
+class Concert(BaseModel):
+    title: str
+    date: str
+    venue: str
+    number_of_tickets: int
+app = FastAPI()
+
+REQUEST_COUNT = Counter("http_requests_total", "Total HTTP requests", ["method", "endpoint", "status"])
+REQUEST_DURATION = Histogram("request_duration_seconds", "Request duration in seconds")
+CPU_USAGE = Gauge("cpu_usage_percent", "CPU usage percent")
+MEMORY_USAGE = Gauge("memory_usage_percent", "Memory usage percent")
+
+
+@app.middleware("http")
+async def metrics_middleware(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time
+    REQUEST_COUNT.labels(request.method, request.url.path, str(response.status_code)).inc()
+    REQUEST_DURATION.observe(duration)
+    return response
+
+@app.post("/buy/{concert_id}")
+def buy_tickets(concert_id: int, purchase: TicketPurchase):
+
+    deny = np.random.randint(low=0, high=1000)
+
+    if deny > 900:
+        print(f" Denying ticket purchase for concert {concert_id} ")
+        return HTTPException(status_code=409, detail="Tickets currently unavailable")
+
+    # Simulate ticket purchasing logic
+    time_to_sleep = np.random.randint(1,50)
+    time.sleep(time_to_sleep)
+    return {"concert_id": concert_id, "quantity": purchase.quantity, "status": "success"}
+
+@app.post("/concerts/new")
+def create_concert(Concert: Concert):
+    # Simulate concert creation logic
+    return {"title": Concert.title, "status": "created"}
+
+@app.get("/metrics")
+def metrics():
+    CPU_USAGE.set(psutil.cpu_percent())
+    MEMORY_USAGE.set(psutil.virtual_memory().percent)
+    return Response(generate_latest(), media_type="text/plain")
---
+++ instructions.md
@@ -0,0 +1,2 @@
+# Instructions
+Follow this tutorial: [here](https://levelup.gitconnected.com/monitoring-fastapi-with-grafana-prometheus-a-5-minute-guide-658280c7f358)
